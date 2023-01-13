6882e6d3c6275925fd2df2a46b25beb4abe003a8
Gornostaeva <kate.gornostaeva@inbox.ru>
2023-01-13 13:38:57 +0300

    start back

---
+++ src/main/java/ru/kate/kurs2back/Kurs2BackApplication.java
@@ -0,0 +1,13 @@
+package ru.kate.kurs2back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2BackApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2BackApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/ru/kate/kurs2back/Kurs2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kate.kurs2back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2BackApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
